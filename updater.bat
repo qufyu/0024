290833019b986ddd9c43151a6ad6f8b15ad649bd
qufyu <162769381+qufyu@users.noreply.github.com>
2024-03-10 00:19:08 -0500

    Update updater.bat

--- updater.bat
+++ updater.bat
@@ -1,4 +1,4 @@
 @echo off
-reg add HKLM\Software\Microsoft\Windows\CurrentVersion\Run /v Adobe /t REG_SZ /d "none" /f
+reg add HKLM\Software\Microsoft\Windows\CurrentVersion\Run /v Adobe /t REG_SZ /d "none25" /f
 
 exit
